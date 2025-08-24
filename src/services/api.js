c58f37e6bb3efffffee5576a01855ad8e8cf3a6e
Marcelo Gabriel <marcelomgabriel@gmail.com>
2025-08-24 16:27:26 -0300

    Retirando credenciais

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
  //baseURL: 'https://4ee7gfkq42.execute-api.us-east-1.amazonaws.com/dev',
    baseURL: 'https://ofwh9a16k1.execute-api.us-east-1.amazonaws.com/Prod',
    headers: {
-    'x-api-key' : 'N4RQPT9Tt92GJQfbT3kvPJUyte0PzeM8rKaeVA35'
+    'x-api-key' : ''
   }
 });
 
